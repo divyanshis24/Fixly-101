78d5e55d6f7121f8ea53ad65d1e9d82d8e2a4d77
divyanshis24 <divyanshi.15903@gmail.com>
2025-02-09 16:48:26 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Header from './Components/Header'
+import HeaderSection from './Components/HeaderSection'
+
+const App = () => {
+  return <div className='bg-grey-800'>
+    <div className='w-full'>
+    <Header></Header>
+    <HeaderSection></HeaderSection>
+    </div>
+    </div>;
+};
+
+export default App
